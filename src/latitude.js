b0468030d30b3c057d474034ac02f27f87ad79e6
zdwolfe <wolfe.zach@gmail.com>
2013-06-11 21:03:07 -0700

    Added (incomplete) function to scrub data from Latitude into something usable by the client-side heatmap library

--- src/latitude.js
+++ src/latitude.js
@@ -1,5 +1,5 @@
 var restler = require('restler');
-var DEBUG = 0;
+var DEBUG = 1;
 var INFO = 1;
 
 function debug(message) {
@@ -26,27 +26,52 @@ function _multiRequest(d, callback, items) {
                 for (var i = 0; i < response.data.items.length; i++) {
                     items.push(resposne.data.items[i]);
                 }
-                return callback(items);
+                return prepareData(callback(items));
             } else {
                 items = items.concat(response.data.items);
                 d.newest = response.data.items[response.data.items.length - 1].timestampMs - 1;
                 return _multiRequest(d, callback, items);
             }
         } else {
-            callback(items);
+            prepareData(callback(items));
         }
     });
 }
 
-function multiRequest(d, callback) { 
-    return _multiRequest(d,callback);
+exports.getData = function(d, callback) { 
+    return prepareData(_multiRequest(d,callback));
+};
+
+function prepareData(data) {
+    debug("prepareData data.length = " + data.length);
+    var retData = [];
+    var item = {};
+    var locations = {};
+    for (var i = 0; i < data.length; i++) {
+        item = {};
+        if (data[i].hasOwnProperty("longitude") && 
+            data[i].hasOwnProperty("latitude")) {
+            if (!locations.hasOwnProperty(data[i].longitude)) {
+                locations[data[i].longitude] = {};
+            }
+            if (!locations[data[i].longitude].hasOwnProperty(data[i].latitude)) {
+                locations[data[i].longitude][data[i].latitude] = {};
+            }
+            var count = locations[data[i].longitude][data[i].latitude].count || 0;
+            locations[data[i].longitude][data[i].latitude].count = count + 1;
+        }
+    }
+    // at this point we have an object (locations) with properties for each longitude. each of 
+    // those properties has a property for each latitude at that longitude
+    // each of those properties has a count
 }
 
 function getRequest(d) {
+    d.baseUrl = d.baseUrl || "https://www.googleapis.com/latitude/v1/location";
     d.newest = d.newest || (new Date()).getTime();
-    d.oldest = d.oldest || 0;
+    d.oldest = d.oldest || d.newest - 2*24*60*60*1000;
     d.granularity = d.granularity || "best";
-    d.fields = d.fields || "items";
+    d.fields = d.fields || "items(latitude%2Clongitude%2CtimestampMs)";
     d.maxresults = d.maxresults || "1000";
 
     if (!d.baseUrl) {  return null; }

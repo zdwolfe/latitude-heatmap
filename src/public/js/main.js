e10d722b8d79f395b2dc751990b79f0ded229c2e
zdwolfe <wolfe.zach@gmail.com>
2013-06-13 19:40:04 -0700

    Adding jquery ui for date picker stuff

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -3,24 +3,24 @@ var heatmap;
 var heatmapData = [];
 
 window.onload = function(){
-	var myLatlng = new google.maps.LatLng(48.3333, 16.35);
-	// sorry - this demo is a beta
-	// there is lots of work todo
-	// but I don't have enough time for eg redrawing on dragrelease right now
-	var myOptions = {
-	  zoom: 2,
-	  center: myLatlng,
-	  mapTypeId: google.maps.MapTypeId.ROADMAP,
-	  disableDefaultUI: false,
-	  scrollwheel: true,
-	  draggable: true,
-	  navigationControl: true,
-	  mapTypeControl: false,
-	  scaleControl: true,
-	  disableDoubleClickZoom: false
-	};
-	map = new google.maps.Map(document.getElementById("heatmapArea"), myOptions);
-	heatmap = new HeatmapOverlay(map, {"radius":15, "visible":true, "opacity":60});
+    var myLatlng = new google.maps.LatLng(48.3333, 16.35);
+    // sorry - this demo is a beta
+    // there is lots of work todo
+    // but I don't have enough time for eg redrawing on dragrelease right now
+    var myOptions = {
+        zoom: 2,
+        center: myLatlng,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        disableDefaultUI: false,
+        scrollwheel: true,
+        draggable: true,
+        navigationControl: true,
+        mapTypeControl: false,
+        scaleControl: true,
+        disableDoubleClickZoom: false
+    };
+    map = new google.maps.Map(document.getElementById("heatmapArea"), myOptions);
+    heatmap = new HeatmapOverlay(map, {"radius":15, "visible":true, "opacity":60});
 };
 
 $("#getData").click(function() {
